dd6c80b541199066bf4ed9c46ffacbdaf47402d4
Christian Di Maio <39567665+gnekt@users.noreply.github.com>
2019-03-19 14:35:45 +0100

    Update queries.sql

--- ex_1/queries.sql
+++ ex_1/queries.sql
@@ -4,3 +4,11 @@ select a.nome,a.cognome,count(b.idbrano) as "Tracks Number"
 from composizione as c, brano as b, artista as a
 where c.idbrano=b.idbrano and c.idartista=230 and c.idartista=a.idartista
 group by(a.idartista)
+
+--Select disc's date and number of disk 
+
+select annodisco,count(iddisco) as "Numero Dischi"
+from disco
+where annodisco is not NULL
+group by(annodisco)
+order by annodisco ASC
