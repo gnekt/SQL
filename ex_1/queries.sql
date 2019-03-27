54940cdc8080bdf49ea525c423837a78b511fb96
Christian Di Maio <39567665+gnekt@users.noreply.github.com>
2019-03-27 13:25:51 +0100

    Update queries.sql

--- ex_1/queries.sql
+++ ex_1/queries.sql
@@ -12,3 +12,31 @@ from disco
 where annodisco is not NULL
 group by(annodisco)
 order by annodisco ASC
+---------------------------------------------
+select  d.iddisco
+from disco as d
+where d.annodisco < any ( select r.annoregistrazione 
+						  from contiene as c inner join registrazione as r
+						   on c.idbrano=r.idbrano and c.idartista = r.idartista 
+						   where d.iddisco = c.iddisco);
+						   
+----------------------------------------------               
+Select Distinct D.*
+From DISCO D,
+CONTIENE C,
+REGISTRAZIONE R
+Where D.IDDISCO = C.IDDISCO
+and C.IDBRANO = R.IDBRANO
+and C.IDARTISTA = R.IDARTISTA
+and R.AnnoRegistrazione > D.AnnoDisco
+order by d.iddisco
+-------------------------------------------------
+
+--I codici ed i titoli dei dischi che
+--contengono brani di cui non si conosce
+--lanno di registrazione
+select d.*,c.*,r.*
+from contiene as c 
+                  inner join disco as d on c.iddisco = d.iddisco
+				  inner join registrazione as r on c.idartista = r.idartista and c.idbrano = r.idbrano
+				  where r.annoregistrazione = 1980
